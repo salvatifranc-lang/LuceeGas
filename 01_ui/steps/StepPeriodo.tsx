48eeaca3418fed9d40181d463d27d92d9cc2dc84
salvatifranc-lang <salvatifranc@gmail.com>
2026-02-01 23:33:04 +0100

    Update StepPeriodo.tsx

--- 01_ui/steps/StepPeriodo.tsx
+++ 01_ui/steps/StepPeriodo.tsx
@@ -1,24 +1,26 @@
+import { useState } from 'react'
 import { Card } from '../components/Card'
 
 type Props = {
-  onNext: () => void
+  onNext: (mesi: number) => void
 }
 
 export function StepPeriodo({ onNext }: Props) {
+  const [mesi, setMesi] = useState(1)
+
   return (
     <Card>
       <h3>Periodo di fatturazione</h3>
 
-      <p>
-        Inserisci il numero di mesi coperti dalla bolletta.
-        <br />
-        <small>Lo trovi in prima pagina.</small>
-      </p>
-
-      <input className="w3-input" type="number" placeholder="Es. 2" />
+      <input
+        className="w3-input"
+        type="number"
+        value={mesi}
+        onChange={e => setMesi(Number(e.target.value))}
+      />
 
       <div style={{ marginTop: 24 }}>
-        <button onClick={onNext}>Avanti</button>
+        <button onClick={() => onNext(mesi)}>Avanti</button>
       </div>
     </Card>
   )
