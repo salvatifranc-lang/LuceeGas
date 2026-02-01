613508f2598c4172f7a71e3ce6d4522bf37b3441
salvatifranc-lang <salvatifranc@gmail.com>
2026-02-01 23:00:21 +0100

    Create index.ts

---
+++ 02_voci_bolletta/index.ts
@@ -0,0 +1,5 @@
+// 02_voci_bolletta/index.ts
+
+export { calcolaMateriaGas } from './materiaGas'
+export { calcolaQuotaFissa } from './quotaFissa'
+export { calcolaImposteProporzionali } from './imposte'
